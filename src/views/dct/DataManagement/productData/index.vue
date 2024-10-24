17b336fe9823caf3ec1c98e4d527105465079706
david.li <david.li@starpavilion-digital.com>
2024-10-24 15:35:20 +0800

    优化查询

--- src/views/dct/DataManagement/productData/index.vue
+++ src/views/dct/DataManagement/productData/index.vue
@@ -4,7 +4,6 @@
     <div class="filter-container">
       <el-row>
 
-
         <el-select v-model="listQuery.product_id" style="margin-left: 20px" multiple collapse-tags  filterable clearable reserve-keyword placeholder="PID">
           <el-option
               v-for="item in pidList"
